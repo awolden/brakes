0c0122118955c9f78b344c1680ac382cf7b2cf75
Alex Wolden <alexander.wolden@nike.com>
2016-04-11 22:26:46 -0700

    examples and fixes

---
+++ examples/example1.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const Brakes = require('../lib/Brakes');
+const timer = 100;
+let successRate = 2;
+let iterations = 0;
+
+function unreliableServiceCall() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      iterations++
+      if (iterations === 10) {
+        successRate = 0.6;
+      }
+      else if (iterations === 100) {
+        successRate = 0.1;
+      }
+      else if (iterations === 200) {
+        successRate = 1;
+      }
+
+
+      if (Math.random() <= successRate) {
+        resolve();
+      }
+      else {
+        reject();
+      }
+    }, timer);
+  });
+}
+
+
+const brake = new Brakes(unreliableServiceCall, {
+  statInterval: 2500,
+  threshold: 0.5,
+  circuitDuration: 15000,
+  timeout: 250
+});
+
+brake.on('snapshot', (stats) => {
+  console.log('Running at:', stats.successful / stats.total);
+  console.log(stats);
+});
+
+brake.on('circuitBroken', () => {
+  console.log('----------Circuit Broken--------------');
+});
+
+brake.on('circuitOpen', () => {
+  console.log('----------Circuit Open--------------');
+});
+
+setInterval(() => {
+  brake.exec()
+    .then(() => {
+      console.log('Succesful');
+    })
+    .catch((err) => {
+      console.log('Failure', err || '');
+    });
+}, 100);
--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 'use strict';
-module.exports = require('./lib');
+module.exports = require('./lib/Brakes');
