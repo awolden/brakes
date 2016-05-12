637fd16cfe81477f8d46fad3d07e19886a10ed40
Sean O'Hollaren <sean.ohollaren@gmail.com>
2016-05-12 13:40:09 -0700

    Add CircuitBrokenError test

---
+++ tests/CircuitBrokenError.spec.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const CircuitBrokenError = require('../lib/CircuitBrokenError');
+const consts = require('../lib/consts');
+const expect = require('chai').expect;
+
+describe('CircuitBrokenError', () => {
+  it('Should be an instance of error', () => {
+    const mockTotals = {
+      failed: 60,
+      timedOut: 0,
+      total: 100,
+      successful: 40
+    };
+    const mockThreshold = 0.5;
+    expect(new CircuitBrokenError(mockTotals, mockThreshold)).to.be.instanceof(Error);
+  });
+  it('Should contain expected properties', () => {
+    const mockTotals = {
+      failed: 60,
+      timedOut: 0,
+      total: 100,
+      successful: 40
+    };
+    const mockThreshold = 0.5;
+    expect(new CircuitBrokenError(mockTotals, mockThreshold)).to.have.a.property('message').that.is.a('string');
+    expect(new CircuitBrokenError(mockTotals, mockThreshold)).to.have.a.property('totals').that.is.an('object');
+  });
+  it('Should have expected error string with calculated failure percentage', () => {
+    const mockTotals = {
+      failed: 60,
+      timedOut: 0,
+      total: 100,
+      successful: 40
+    };
+    const mockThreshold = 0.5;
+    expect(new CircuitBrokenError(mockTotals, mockThreshold)).to.have.a.property('message').that.equals(`${consts.CIRCUIT_BROKEN} - The percentage of failed requests (60%) is greater than the threshold specified (50%)`);
+  });
+
+});
