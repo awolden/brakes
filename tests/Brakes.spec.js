e38b6d375abdc6fb83d5c741df197318a34b23d1
Alex Wolden <woldena@ochin.org>
2016-05-14 23:56:12 -0700

    fixed brakes instance destruction

--- tests/Brakes.spec.js
+++ tests/Brakes.spec.js
@@ -8,11 +8,14 @@ const sinon = require('sinon');
 const TimeOutError = require('../lib/TimeOutError');
 const CircuitBrokenError = require('../lib/CircuitBrokenError');
 
+let brake;
+
 const defaultOptions = {
   bucketSpan: 1000,
   bucketNum: 60,
   name: 'defaultBrake',
   group: 'defaultBrakeGroup',
+  registerGlobal: true,
   circuitDuration: 30000,
   statInterval: 1200,
   startDelay: 5000,
@@ -46,12 +49,18 @@ const fbpr = function fallback(foo, err) {
   });
 };
 describe('Brakes Class', () => {
+  afterEach(() => {
+    if (brake instanceof Brakes) {
+      brake.destroy();
+      brake = undefined;
+    }
+  });
   it('Should be an instance of EventEmitter', () => {
-    const brake = new Brakes(noop);
+    brake = new Brakes(noop);
     expect(brake).to.be.instanceof(EventEmitter);
   });
   it('Should be instantiated with default options', () => {
-    const brake = new Brakes(noop);
+    brake = new Brakes(noop);
     // const snapshotSpy = sinon.spy(brake._stats, 'startSnapshots');
     // const statsSpy = sinon.spy(brake, '_startStatsCheck');
     // expect(snapshotSpy.calledOnce).to.equal(true);
@@ -60,26 +69,26 @@ describe('Brakes Class', () => {
     expect(brake._opts).to.deep.equal(defaultOptions);
   });
   it('Should promisify the service func', () => {
-    const brake = new Brakes(noop);
+    brake = new Brakes(noop);
     return brake._serviceCall('test').then((result) => {
       expect(result).to.equal('test');
     });
   });
   it('Should promisify and reject service func', () => {
-    const brake = new Brakes(noop);
+    brake = new Brakes(noop);
     return brake._serviceCall(null, 'err').then(null, (err) => {
       expect(err).to.be.instanceof(Error);
       expect(err.message).to.equal('err');
     });
   });
   it('Should accept a promise', () => {
-    const brake = new Brakes(nopr);
+    brake = new Brakes(nopr);
     return brake._serviceCall('test').then((result) => {
       expect(result).to.equal('test');
     });
   });
   it('Should reject a promise', () => {
-    const brake = new Brakes(nopr);
+    brake = new Brakes(nopr);
     return brake._serviceCall(null, 'err').then(null, (err) => {
       expect(err).to.be.instanceof(Error);
       expect(err.message).to.equal('err');
@@ -87,7 +96,7 @@ describe('Brakes Class', () => {
   });
   it('Throw an error if not passed a function', () => {
     expect(() => {
-      const brake = new Brakes();
+      brake = new Brakes();
       brake.test();
     }).to.throw();
   });
@@ -95,14 +104,14 @@ describe('Brakes Class', () => {
     const overrides = {
       name: 'allYourNameAreBelongToUs'
     };
-    const brake = new Brakes(noop, overrides);
+    brake = new Brakes(noop, overrides);
     expect(brake.name).to.deep.equal(overrides.name);
   });
   it('Should be instantiated with a group', () => {
     const overrides = {
       group: 'allYourGroupAreBelongToUs'
     };
-    const brake = new Brakes(noop, overrides);
+    brake = new Brakes(noop, overrides);
     expect(brake.group).to.deep.equal(overrides.group);
   });
   it('Should be instantiated with override options', () => {
@@ -111,17 +120,18 @@ describe('Brakes Class', () => {
       bucketNum: 601,
       circuitDuration: 300001,
       statInterval: 1,
+      registerGlobal: false,
       name: 'PUT:/path',
       group: 'fakeGroup',
       startDelay: 50010,
       threshold: 0.3,
       timeout: 100
     };
-    const brake = new Brakes(noop, overrides);
+    brake = new Brakes(noop, overrides);
     expect(brake._opts).to.deep.equal(overrides);
   });
   it('Should Resolve a service call and trigger event', () => {
-    const brake = new Brakes(nopr);
+    brake = new Brakes(nopr);
     const spy = sinon.spy(() => {});
     brake.on('success', spy);
     return brake.exec('foo').then(result => {
@@ -130,7 +140,7 @@ describe('Brakes Class', () => {
     });
   });
   it('Should Reject a service call and trigger event', () => {
-    const brake = new Brakes(noop);
+    brake = new Brakes(noop);
     const spy = sinon.spy(() => {});
     brake.on('failure', spy);
     return brake.exec(null, 'err').then(null, err => {
@@ -140,7 +150,7 @@ describe('Brakes Class', () => {
     });
   });
   it('Should timeout a service call and trigger event', () => {
-    const brake = new Brakes(slowpr, {
+    brake = new Brakes(slowpr, {
       timeout: 1
     });
     const spy = sinon.spy(() => {});
@@ -151,14 +161,14 @@ describe('Brakes Class', () => {
     });
   });
   it('Should auto reject if circuit is broken', () => {
-    const brake = new Brakes(nopr);
+    brake = new Brakes(nopr);
     brake._circuitOpen = true;
     return brake.exec(null, 'err').then(null, err => {
       expect(err).to.be.instanceof(CircuitBrokenError);
     });
   });
   it('Should call fallback if circuit is broken', () => {
-    const brake = new Brakes(nopr);
+    brake = new Brakes(nopr);
     brake.fallback(fbpr);
     brake._circuitOpen = true;
     return brake.exec('test').then(result => {
@@ -166,7 +176,7 @@ describe('Brakes Class', () => {
     });
   });
   it('Fallback should cascade fail', () => {
-    const brake = new Brakes(nopr);
+    brake = new Brakes(nopr);
     brake.fallback(noop);
     return brake.exec(null, 'err').then(null, err => {
       expect(err).to.be.instanceof(Error);
@@ -174,14 +184,14 @@ describe('Brakes Class', () => {
     });
   });
   it('Fallback should succeed', () => {
-    const brake = new Brakes(nopr);
+    brake = new Brakes(nopr);
     brake.fallback(fbpr);
     return brake.exec(null, 'thisShouldFailFirstCall').then(result => {
       expect(result).to.equal('thisShouldFailFirstCall');
     });
   });
   it('_open should open', (done) => {
-    const brake = new Brakes(nopr, {
+    brake = new Brakes(nopr, {
       circuitDuration: 10
     });
 
@@ -212,7 +222,7 @@ describe('Brakes Class', () => {
 
   });
   it('_close should close', () => {
-    const brake = new Brakes(nopr);
+    brake = new Brakes(nopr);
     brake._circuitOpened = true;
     const funcSpy = sinon.spy(brake, '_startStatsCheck');
     const eventSpy = sinon.spy(() => {});
@@ -223,7 +233,7 @@ describe('Brakes Class', () => {
     expect(eventSpy.calledOnce).to.equal(true);
   });
   it('_startStatsCheck should set flag after delay', (done) => {
-    const brake = new Brakes(nopr, {
+    brake = new Brakes(nopr, {
       startDelay: 1
     });
     brake._stopStatsCheck();
@@ -233,9 +243,10 @@ describe('Brakes Class', () => {
     }, 5);
   });
   it('_snapshotHandler should transform stats object and emit', (done) => {
-    const brake = new Brakes(nopr, {
+    brake = new Brakes(nopr, {
       name: 'brake1',
-      group: 'brakeGroup1'
+      group: 'brakeGroup1',
+      registerGlobal: false
     });
     const eventSpy = sinon.spy(() => {});
     brake.on('snapshot', eventSpy);
@@ -257,12 +268,12 @@ describe('Brakes Class', () => {
     }, 5);
   });
   it('_stopStatsCheck should set flag', () => {
-    const brake = new Brakes(nopr);
+    brake = new Brakes(nopr);
     brake._stopStatsCheck();
     expect(brake._checkingStatus).to.equal(false);
   });
   it('destroy() should remove all references', () => {
-    const brake = new Brakes(nopr);
+    brake = new Brakes(nopr);
 
     // first test that we are handling all appropriate events
     const expectedEvents = ['success', 'timeout', 'failure', 'snapshot'];
@@ -271,25 +282,26 @@ describe('Brakes Class', () => {
     expect(Object.keys(brake._events).length).to.equal(expectedEvents.length);
 
     const deregisterStub = sinon.stub(globalStats, 'deregister');
-    const removeEventStub = sinon.stub(brake, 'removeAllListeners');
-
+    const removeEventStub = sinon.stub(brake, 'removeAllListeners', () => true);
     brake.destroy();
     expect(deregisterStub.calledOnce).to.equal(true);
     expect(removeEventStub.callCount).to.equal(actualEvents.length);
+    brake.removeAllListeners.restore();
+    globalStats.deregister.restore();
   });
   it('getGlobalStats should return instance of globalStats', () => {
-    const brake = new Brakes(nopr);
+    brake = new Brakes(nopr);
     expect(brake.getGlobalStats()).to.equal(globalStats);
     expect(Brakes.getGlobalStats()).to.equal(globalStats);
   });
   it('_checkStats should not check when status flag is false', () => {
-    const brake = new Brakes(nopr);
+    brake = new Brakes(nopr);
     const spy = sinon.spy(brake, '_close');
     brake._checkStats();
     expect(spy.calledOnce).to.equal(false);
   });
   it('_checkStats should not check when total is 0', () => {
-    const brake = new Brakes(nopr);
+    brake = new Brakes(nopr);
     brake._checkingStatus = true;
     const spy = sinon.spy(brake, '_close');
     brake._checkStats({
@@ -298,7 +310,7 @@ describe('Brakes Class', () => {
     expect(spy.calledOnce).to.equal(false);
   });
   it('_checkStats should not check when circuit is broken is 0', () => {
-    const brake = new Brakes(nopr);
+    brake = new Brakes(nopr);
     brake._checkingStatus = true;
     brake._closed = true;
     const spy = sinon.spy(brake, '_close');
@@ -308,7 +320,7 @@ describe('Brakes Class', () => {
     expect(spy.calledOnce).to.equal(false);
   });
   it('_checkStats should check and not close', () => {
-    const brake = new Brakes(nopr);
+    brake = new Brakes(nopr);
     const spy = sinon.spy(brake, '_close');
     brake._checkStats({
       successful: 10,
@@ -317,7 +329,7 @@ describe('Brakes Class', () => {
     expect(spy.calledOnce).to.equal(false);
   });
   it('_checkStats should check and not close', () => {
-    const brake = new Brakes(nopr);
+    brake = new Brakes(nopr);
     brake._checkingStatus = true;
     const spy = sinon.spy(brake, '_open');
     brake._checkStats({
