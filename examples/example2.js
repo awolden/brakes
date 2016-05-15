98ff5c158b81c3af4615c0267df81cae6f55b263
Alex Wolden <woldena@ochin.org>
2016-05-14 21:34:49 -0700

    added example for demonstrating global stats

---
+++ examples/example2.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const Brakes = require('../lib/Brakes');
+const timer = 100;
+let successRate = 2;
+let iterations = 0;
+
+function unreliableServiceCall() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      iterations++;
+      if (iterations === 10) {
+        successRate = 0.6;
+      }
+      else if (iterations === 100) {
+        successRate = 0.1;
+      }
+      else if (iterations === 200) {
+        successRate = 1;
+      }
+
+
+      if (Math.random() <= successRate) {
+        resolve();
+      }
+      else {
+        reject();
+      }
+    }, timer);
+  });
+}
+
+
+const brake = new Brakes(unreliableServiceCall, {
+  statInterval: 2500,
+  threshold: 0.5,
+  circuitDuration: 15000,
+  timeout: 250
+});
+
+const globalStats = Brakes.getGlobalStats();
+
+globalStats.getRawStream().on('data', (data) => {
+  console.log(data.toString());
+});
+
+setInterval(() => {
+  brake.exec()
+    .then(() => {
+      console.log('Succesful');
+    })
+    .catch((err) => {
+      console.log('Failure', err || '');
+    });
+}, 100);
